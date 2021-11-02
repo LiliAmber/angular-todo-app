c95d25dc8c80182b86b5e39901cf5ab2b0864e00
LiliAmber <raradanniswaramusin@MacBook-Pro-Rara.local>
2021-11-02 12:16:07 +0700

    apply requireHttps

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-// app.use(requireHTTPS) //komen line ini kalo run di local
+app.use(requireHTTPS) //komen line ini kalo run di local
 app.use(express.static('./dist/todo'))
 
 app.get('/*', (req, res) => 
